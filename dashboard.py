5699633178f227f4a06f859c9b5e74ff335829ff
jtchan17 <jiatongchan0217@gmail.com>
2024-06-03 23:30:30 +0800

    update filter function

--- dashboard.py
+++ dashboard.py
@@ -34,14 +34,6 @@ with st.sidebar:
 #####################################################################
 
 #####################################################################
-# Graph/Chart/Plot
-
-#Sentiment Pie chart
-    
-
-#####################################################################
-
-#####################################################################
 #   Dashboard
 st.title('📈 Sentiment Analyzer :blue[Dashboard] of Stock Prices')
 
@@ -94,10 +86,15 @@ def query(table, year, companies):
     query = f'SELECT * FROM dashboard.{table} WHERE '
     if year != 'All':
         query += f'YEAR(published_date) = {year} ' if table == 'financialnews' else f'YEAR(date) = {year} '
-    if companies:
-        companies_str = ', '.join(f'"{company}"' for company in companies)
-        query += f'AND company IN ({companies_str})'
-    return query
+        if companies:
+            companies_str = ', '.join(f'"{company}"' for company in companies)
+            query += f'AND company IN ({companies_str})'
+        return query
+    else:
+        if companies:
+            companies_str = ', '.join(f'"{company}"' for company in companies)
+            query += f'company IN ({companies_str})'
+        return query
 
 # List of selected companies
 selected_companies = [company for company, selected in companies.items() if selected]
@@ -124,7 +121,7 @@ with r1c1:
 
 with r1c2:
     st.subheader('Highest Price Across Years')
-    query = 'SELECT YEAR(date) as year, company, MAX(high) AS Highest FROM dashboard.stockprice WHERE YEAR(date) in (2021, 2022, 2023) GROUP BY company, year ORDER BY Highest DESC;'
+    query = 'SELECT YEAR(date) as Year, company AS Companies, MAX(high) AS Highest FROM dashboard.stockprice WHERE YEAR(date) in (2021, 2022, 2023) GROUP BY Companies, Year ORDER BY Year DESC, Highest DESC;'
     df_highest = conn.query(query, ttl=600)
     st.table(df_highest)
 
@@ -157,23 +154,23 @@ r3c1, r3c2 = st.columns((3, 10), gap='small')
 
 with r3c1:
     st.subheader('Top 10 Publishers :newspaper:')
-    df_fn2 = (filtered_df_fn.groupby('publisher').size().reset_index(name='Total'))
-    df_fn2 = (df_fn2.sort_values(by="Total", ascending=False)).head(10)
-    st.dataframe(df_fn2,
+    df_fn1 = (filtered_df_fn.groupby('publisher').size().reset_index(name='Total'))
+    df_fn1 = (df_fn1.sort_values(by="Total", ascending=False)).head(10)
+    st.dataframe(df_fn1,
                  column_order=("publisher", "Total"),
                  hide_index=True,
                  width=None,
                  column_config={
                      "publisher": st.column_config.TextColumn("Publisher",),
-                     "Total": st.column_config.ProgressColumn("Total",format="%f",min_value=0,max_value=max(df_fn2.Total),)
+                     "Total": st.column_config.ProgressColumn("Total",format="%f",min_value=0,max_value=max(df_fn1.Total),)
                      }
                  )
 
 with r3c2:
     st.subheader('Publishers :newspaper:')
-    df_fn2 = filtered_df_fn.groupby('publisher').size().reset_index(name='Total')
-    fig = px.bar(df_fn2,x='Total', y='publisher', template='seaborn')
-    fig.update_traces(text=df_fn2['publisher'], textposition='inside')
+    df_fn1 = filtered_df_fn.groupby('publisher').size().reset_index(name='Total')
+    fig = px.bar(df_fn1,x='Total', y='publisher', template='seaborn')
+    fig.update_traces(text=df_fn1['publisher'], textposition='inside')
     st.plotly_chart(fig, use_container_width=True, height = 500)
 
 pricing_data, news = st.tabs(['Pricing Data', 'News'])
@@ -181,13 +178,14 @@ pricing_data, news = st.tabs(['Pricing Data', 'News'])
 with pricing_data:
     st.subheader('Price Movements')
     with st.container(height=500, border=True):
-        st.table(df_sp)
+        st.table(filtered_df_sp)
     csv=filtered_df_sp.to_csv(index = False).encode('utf-8')
     st.download_button(label='Download Historical Data', data= csv, file_name='Historical Data.csv')
 
 with news:
     st.subheader('News')
     with st.container(height=500, border=True):
+        df_fn1 = filtered_df_fn.sort_values(by="published_date", ascending=True)
         st.table(df_fn)
     csv=filtered_df_fn.to_csv(index = False).encode('utf-8')
     st.download_button(label='Download Financial News', data= csv, file_name='Financial News.csv')
